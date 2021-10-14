9727197bb2ebdb04bc217cea205eb269fc0ea0bc
woodystock <91628945+woodystock@users.noreply.github.com>
2021-10-14 10:19:19 +0100

    Updated comment

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -9,7 +9,7 @@ function processTransactions(transactions) {
         Count[j] ? (Count[j]) += 1 : Count[j] = 1; // frequency of each item
 
     const SortbyNumber = Object.entries(Count); // Object [item, frequency]​
-    SortbyNumber.sort((a, b) => b[1] - a[1] || -(a[0] < b[0])); // Sort based on frequency
+    SortbyNumber.sort((a, b) => b[1] - a[1] || -(a[0] < b[0])); // Sort based on 'frequency' then 'name' alphabetically
     
     return SortbyNumber.map( Element => Element.join(" ")); // Map into a single array
 }
