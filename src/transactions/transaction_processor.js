a7e1d79c72d2313c5b6f95753300fdb5ca2c9872
woodystock <91628945+woodystock@users.noreply.github.com>
2021-10-14 10:05:53 +0100

    cleanup + format

--- src/transactions/transaction_processor.js
+++ src/transactions/transaction_processor.js
@@ -1,18 +1,17 @@
-const processTransactions = (transactions) => {
+function processTransactions(transactions) {
 
     if (!Array.isArray(transactions) || transactions.includes(undefined)) {
         throw new Error("Undefined collection of transactions")
     }
 
     const Count = {};
-      for (let j of transactions) 
-        Count[j] ? (Count[j] += 1) : (Count[j] = 1); // frequency of each item
+    for (let j of transactions)
+        Count[j] ? (Count[j]) += 1 : Count[j] = 1; // frequency of each item
 
-    const SortbyNumber = Object.entries(Count); // Object [item, frequency]
-    ​
+    const SortbyNumber = Object.entries(Count); // Object [item, frequency]​
     SortbyNumber.sort((a, b) => b[1] - a[1] || -(a[0] < b[0])); // Sort based on frequency
     
-    return SortbyNumber.map((Element) => Element.join(" ")); // Map into a single array
+    return SortbyNumber.map( Element => Element.join(" ")); // Map into a single array
 }
 
 module.exports = processTransactions;
